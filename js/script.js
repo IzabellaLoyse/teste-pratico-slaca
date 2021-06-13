84b9f8fca52e971661f3701af94c34a5c8fcd94c
Izabella Loyse <izabellaloyse13@gmail.com>
2021-06-13 01:03:29 -0300

    feat(menu): Implementação do Menu Ativo do Sidebar left

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 
+/*=== Add and remove asset class from sidebar menu === */
+
+
+const itemMenu = document.querySelectorAll(".c-menu__item");
+
+function addAssetClass(item) {
+	itemMenu.forEach((item) => {
+		item.classList.remove("c-menu__item--active");
+	});
+
+	item.classList.add("c-menu__item--active");
+}
+
+itemMenu.forEach((item) => {
+	item.addEventListener("click", () => {
+		addAssetClass(item);
+	});
+});
