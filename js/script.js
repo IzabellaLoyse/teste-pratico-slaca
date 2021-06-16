eb9cf9a42a3325018c1fcc5d25a1762b3a34cd3e
Izabella Loyse <izabellaloyse13@gmail.com>
2021-06-15 22:37:09 -0300

    feat(projeto): Implementação de JavaScript na seção Resumo e criação/ estilização da seção Discussões

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
-
 /*=== Add and remove asset class from sidebar menu === */
 
-
 const itemMenu = document.querySelectorAll(".c-menu__item");
+const readMore = document.querySelector(".c-read-more");
+
 
 function addAssetClass(item) {
 	itemMenu.forEach((item) => {
-		item.classList.remove("c-menu__item--active");
+		item.classList.remove("is-menu__item--active");
 	});
 
-	item.classList.add("c-menu__item--active");
+	item.classList.add("is-menu__item--active");
 }
 
 itemMenu.forEach((item) => {
 	item.addEventListener("click", () => {
 		addAssetClass(item);
 	});
+});
+
+
+
+
+
+const showMore = () => {
+	const paragraphs = document.querySelectorAll(
+		".is-main-box__text--visibility");
+	const spanVisibility = document.querySelector("#js-span-visibility");
+
+	spanVisibility.remove();
+
+	paragraphs.forEach((p) =>
+		p.classList.remove("is-main-box__text--visibility")
+	);
+};
+
+readMore.addEventListener("click", () => {
+	showMore();
 });
