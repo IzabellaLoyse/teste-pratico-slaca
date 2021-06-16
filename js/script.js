5556d6cc359b94628883e8b66ab9ad9bbda03ec4
Izabella Loyse <izabellaloyse13@gmail.com>
2021-06-16 15:21:35 -0300

    feat(projeto): Finalização da estilização da seção Discussões

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-/*=== Add and remove asset class from sidebar menu === */
-
 const itemMenu = document.querySelectorAll(".c-menu__item");
 const readMore = document.querySelector(".c-read-more");
+const createTopicButton = document.querySelector(".c-discussion-box__button");
 
 
+
+/*=== Add and remove asset class from sidebar menu === */
 function addAssetClass(item) {
 	itemMenu.forEach((item) => {
 		item.classList.remove("is-menu__item--active");
@@ -20,12 +21,11 @@ itemMenu.forEach((item) => {
 
 
 
-
-
 const showMore = () => {
 	const paragraphs = document.querySelectorAll(
-		".is-main-box__text--visibility");
-	const spanVisibility = document.querySelector("#js-span-visibility");
+		".is-main-box__text--visibility"
+	);
+	const spanVisibility = document.querySelector(".js-span-visibility");
 
 	spanVisibility.remove();
 
@@ -36,4 +36,18 @@ const showMore = () => {
 
 readMore.addEventListener("click", () => {
 	showMore();
+});
+
+
+
+const formVisibility = () => {
+	const removeFormVisibility = document.querySelector(".js-form-box");
+	const createTopic = document.querySelector(".js-discussion-box");
+
+	removeFormVisibility.classList.remove("is-discussion-box--visibility");
+	createTopic.classList.add("is-discussion-box--visibility");
+};
+
+createTopicButton.addEventListener("click", () => {
+	formVisibility();
 });
